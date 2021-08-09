4aa858215cc529cd0f1822e342192be8b97144af
ltsato <82470260+ltsato@users.noreply.github.com>
2021-08-09 18:30:34 -0300

    Update App.controller.js

--- App.controller.js
+++ App.controller.js
@@ -2,6 +2,10 @@ sap.ui.define([
    "sap/ui/core/mvc/Controller"
 ], function (Controller) {
    "use strict";
-   return Controller.extend("", {
+   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onShowHello : function () {
+         // show a native JavaScript alert
+         alert("Hello World");
+      }
    });
 });
