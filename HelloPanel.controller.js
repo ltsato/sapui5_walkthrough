5eb64bd085dcd52222cfb5d567f24ba776f11161
ltsato <82470260+ltsato@users.noreply.github.com>
2021-08-25 17:34:16 -0300

    Update HelloPanel.controller.js

--- HelloPanel.controller.js
+++ HelloPanel.controller.js
@@ -21,6 +21,7 @@ sap.ui.define([
 				this.pDialog = Fragment.load({
 					id: oView.getId(),
 					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+					controller: this
 				}).then(function (oDialog) {
 					// connect dialog to the root view of this component (models, lifecycle)
 					oView.addDependent(oDialog);
@@ -30,6 +31,13 @@ sap.ui.define([
 			this.pDialog.then(function(oDialog) {
 				oDialog.open();
 			});
+		},
+
+		onCloseDialog : function () {
+			// note: We don't need to chain to the pDialog promise, since this event-handler
+			// is only called from within the loaded dialog itself.
+			this.byId("helloDialog").close();
 		}
+	   
    });
 });
