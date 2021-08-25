1f5493016117b65f6d2dd6dbf16cae0d8a3e7770
ltsato <82470260+ltsato@users.noreply.github.com>
2021-08-25 17:00:26 -0300

    Update HelloPanel.controller.js

--- HelloPanel.controller.js
+++ HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+   "sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
       onShowHello : function () {
